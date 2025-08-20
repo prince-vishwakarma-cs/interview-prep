25db7a0cff20fb5d8751319e10e637b1a2758c15
prince-vishwakarma-cs <pv913177@gmail.com>
2025-08-20 08:52:01 +0530

    fixed cookies

--- server-vercel/utils/variables.js
+++ server-vercel/utils/variables.js
@@ -14,7 +14,7 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 
 export const NODE_ENV = process.env.NODE_ENV;
 
-export const COOKIE_SAME_SITE = process.env.COOKIE_SAME_SITE || "Strict";
+export const COOKIE_SAME_SITE = process.env.COOKIE_SAME_SITE || "None";
 
 export const COOKIE_MAX_AGE = process.env.COOKIE_MAX_AGE || 7 * 24 * 60 * 60 * 1000;
 
